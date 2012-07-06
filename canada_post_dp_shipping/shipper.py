74487c4dec6833e20a87dc099ee0cbdf894b43ff
Tomas Neme <lacrymology@gmail.com>
2012-07-06 19:52:00 -0300

    start working on shipper class

--- canada_post_dp_shipping/shipper.py
+++ canada_post_dp_shipping/shipper.py
@@ -7,44 +7,60 @@ This dummy module can be used as a basis for creating your own
 
 # Note, make sure you use decimal math everywhere!
 from decimal import Decimal
+import logging
 from django.utils.translation import ugettext as _
 from shipping.modules.base import BaseShipper
 
+log = logging.getLogger(__file__)
+
 class Shipper(BaseShipper):
 
-    flatRateFee = Decimal("15.00")
-    id = "Dumy"
-        
+    def __init__(self, cart=None, contact=None, service_type=None):
+        """
+        Initialize a Shipper for a given service type
+        """
+        if service_type:
+            self.service_code, self.service_text = service_type
+        else:
+            self.service_code = "BAD.CODE"
+            self.service_text = "Uninitialized"
+        self.id = "canadapost-dev-prog-{}".format(self.service_text)
+        super(BaseShipper, self).__init__(cart=cart, contact=contact)
+
     def __str__(self):
         """
         This is mainly helpful for debugging purposes
         """
-        return "Dummy Flat Rate"
+        return "Canada Post Developer Program"
         
     def description(self):
         """
         A basic description that will be displayed to the user when selecting their shipping options
         """
-        return _("Dummy Flat Rate Shipping")
+        return _("Canada Post - {service_type}".format(
+            service_type=self.service_text))
 
     def cost(self):
         """
         Complex calculations can be done here as long as the return value is a decimal figure
         """
-        assert(self._calculated)
-        return(self.flatRateFee)
+        assert self._calculated
+        return self.charges
 
     def method(self):
         """
         Describes the actual delivery service (Mail, FedEx, DHL, UPS, etc)
         """
-        return _("US Mail")
+        return _("Canada Post")
 
     def expectedDelivery(self):
         """
         Can be a plain string or complex calcuation returning an actual date
         """
-        return _("3 - 4 business days")
+        if self.transit_time is not None:
+            return _("{days} business days".format(days=self.transit_time))
+        else:
+            return _("Unknown")
 
     def valid(self, order=None):
         """
