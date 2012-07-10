2ec736d4ace9df78cc8ba2d7dfc626f3ba326010
Tomas Neme <lacrymology@gmail.com>
2012-07-10 14:51:31 -0300

    multiple loading brings issues

--- canada_post_dp_shipping/__init__.py
+++ canada_post_dp_shipping/__init__.py
@@ -1,5 +1,8 @@
 from livesettings.functions import config_choice_values
+from satchmo_utils import load_once
 import shipper
+
+load_once('canada_post_dp_shipping', 'canada_post_dp_shipping')
 
 def get_methods():
     return [shipper.Shipper(service_type=value)
