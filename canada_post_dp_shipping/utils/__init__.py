03140431d90f5b023eff6d203ceeb9872558da47
Tomas Neme <lacrymology@gmail.com>
2012-08-01 14:45:53 -0300

    create Destination object from contact data

--- canada_post_dp_shipping/utils/__init__.py
+++ canada_post_dp_shipping/utils/__init__.py
@@ -6,3 +6,14 @@ def get_origin(shop_details):
                   address=(shop_details.street1, shop_details.street2),
                   city=shop_details.city, province=shop_details.state)
 
+def get_destination(contact):
+    return Destination(postal_code=contact.shipping_address.postal_code,
+                       country_code=contact.shipping_address.country.iso2_code,
+                       name="{last_name}, {first_name}".format(
+                           first_name=contact.first_name,
+                           last_name=contact.last_name),
+                       address=(contact.shipping_address.street1,
+                                contact.shipping_address.street2),
+                       city=contact.shipping_address.city,
+                       province=contact.shipping_address.state)
+
