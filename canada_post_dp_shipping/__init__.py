868a52e8500275182e99dbd034ad46490ce4c7b9
Tomas Neme <lacrymology@gmail.com>
2012-09-11 16:13:15 -0300

    avoid circular imports

--- canada_post_dp_shipping/__init__.py
+++ canada_post_dp_shipping/__init__.py
@@ -1,10 +1,10 @@
 from livesettings.functions import config_choice_values
 from satchmo_utils import load_once
-import shipper
 
 load_once('canada_post_dp_shipping', 'canada_post_dp_shipping')
 
 def get_methods():
+    import shipper
     return [shipper.Shipper(service_type=value)
             for value in config_choice_values('canada_post_dp_shipping',
                                               'SHIPPING_CHOICES')]
