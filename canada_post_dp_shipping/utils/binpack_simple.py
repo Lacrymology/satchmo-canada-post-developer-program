c02bbaeed8f71b64cf787bdbceb5ab5bc1a9e9e8
Tomas Neme <lacrymology@gmail.com>
2013-04-11 15:40:25 -0300

    small error in logic fixed

--- canada_post_dp_shipping/utils/binpack_simple.py
+++ canada_post_dp_shipping/utils/binpack_simple.py
@@ -244,11 +244,11 @@ def iterate_permutations(original_packages, bins, iterlimit):
         # the cost is the sum of every bigger bin's packaging cost without the
         #  last box (since it was passed on the following
         if ix > 0:
-            cost += sum(packing_cost(ps, b) for ps, b in packlist[ix-1][:-1])
-            for ps, b in packlist[ix-1][:-1]:
+            cost += sum(packing_cost(ps, b) for ps, b in packlist[-1][:-1])
+            for ps, b in packlist[-1][:-1]:
                 newpacks.append((ps, b))
 
-            ps, b = packlist[ix-1][-1]
+            ps, b = packlist[-1][-1]
             if ps[:-1]:
                 cost += packing_cost(ps[:-1], b)
                 newpacks.append((ps[:-1], b))
